d53e52f9d4a5547158f72bff357835ee7e359a20
nmhudson2 <91906231+nmhudson2@users.noreply.github.com>
2024-03-24 11:06:13 -0400

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+// Base route calls the PageRouteController class, which will redirect based on the incomming request. 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
